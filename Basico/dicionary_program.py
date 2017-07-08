bb6689e15fcc6501d1e8f2d171384a26cac02c2d
Luiz Rubens <luiz.romera@fatec.sp.gov.br>
2017-07-08 15:01:56 -0300

    Programa manipulador de Strings

---
+++ Basico/dicionary_program.py
@@ -0,0 +1,2 @@
+luiz_rubens=('cidade')
+print luiz_rubens;
---
+++ Basico/string_program.py
@@ -0,0 +1,12 @@
+## Programa que trabalha com strings.
+
+meu_nome = "Luiz Rubens Ribas Romera"
+## print meu_nome[1:10]
+
+## print meu_nome[23::-1]   
+
+## meu_nome += meu_nome
+
+## print meu_nome
+
+print len(meu_nome)
